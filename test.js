88077a6895aea90717c7552bd97ea9102e9dd354
Rick42Morty <evgenevgen00@mail.ru>
2018-10-28 01:36:26 +0300

    feat: add make function

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const sumOfOther = require('./src/sumOfOther.js');
+const make = require('./src/make.js');
 
-describe('sumOfOther', () => {
+describe('sumOfOther function', () => {
   it('Should work with valid array', () => {
     const arr = sumOfOther([2, 3, 4, 1]);
     assert.deepEqual(arr, [8, 7, 6, 9]);
@@ -14,4 +15,39 @@ describe('sumOfOther', () => {
     const arr = sumOfOther([2, 3, 4, 1]);
     assert.deepEqual(arr, [8, 7, 6, 9]);
   });
+});
+
+describe('make function', () => {
+  it('Should work with sum function', () => {
+    function sum(a, b) {
+      return a + b;
+    }
+    const ans = make(15)(34, 21, 666)(41)(sum);
+    assert.deepEqual(ans, 777);
+  });
+
+  it('Should return "not enough arguments" message if number of number args is less than two', () => {
+    function sum(a, b) {
+      return a + b;
+    }
+    const ans = make(15)(sum);
+    assert.deepEqual(ans, "not enough arguments");
+  });
+
+  it('Should work fine with two number arguments', () => {
+    function sum(a, b) {
+      return a + b;
+    }
+    const ans = make(21, 21)(sum);
+    assert.deepEqual(ans, 42);
+  });
+
+  it('Should work with product function', () => {
+    function prod(a, b) {
+      return a * b;
+    }
+    const ans = make(5)(3, 4)(1, 2)(1, 1, 1)(7)(prod);
+    assert.deepEqual(ans, 840);
+  });
+
 });
